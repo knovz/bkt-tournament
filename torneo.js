5524bbb9713e59f6b278a89126e9a6ef1c3bae4c
KnoVz <hknovz@gmail.com>
2015-12-23 08:06:08 +0100

    permite modificaciones en lista de partidos

--- torneo.js
+++ torneo.js
@@ -145,8 +145,33 @@ if (Meteor.isClient) {
             event.preventDefault();
             var score1 = event.target.score1.value;
             var score2 = event.target.score2.value;
+            
+            var score1 = event.target.score1.value;
+            var score2 = event.target.score2.value;
+            
+            var date = (event.target.date.value.length === 0) ? "20016-01-02" : event.target.date.value;
+            var time = (event.target.time.value.length === 0) ? "00:00" : event.target.time.value;
+            var pitch = (event.target.pitch.value.length === 0) ? "A" : event.target.pitch.value;
+            var matchnum = (event.target.matchnum.value.length === 0) ? "1" : event.target.matchnum.value;
+            var team1 = (event.target.team1.value.length === 0) ? "team 1" : event.target.team1.value;
+            var team2 = (event.target.team2.value.length === 0) ? "team 2" : event.target.team2.value;
+            var category = (event.target.category.value.length === 0) ? "no category" : event.target.category.value;
+            var level = (event.target.level.value.length === 0) ? "A" : event.target.level.value;
+
+           
             Matches.update(this._id, {
-                $set: {score1: score1, score2: score2}
+                $set: {
+                    date: date,
+                    time: time,
+                    pitch: pitch,
+                    matchnum: matchnum,
+                    team1: team1,
+                    team2: team2,
+                    category: category,
+                    level: level,
+                    score1: score1,
+                    score2: score2
+                }
             });
         }
     });
